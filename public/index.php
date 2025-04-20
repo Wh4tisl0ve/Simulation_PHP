14f66aaf80519ce6eb7cbd8968eab61381c2fe63
Vladislav <whatislov@mail.ru>
2025-04-20 13:20:45 +0300

    refactor: remove excess space for public folder

--- public/index.php
+++ public/index.php
@@ -17,24 +17,17 @@ use App\Simulation;
 
 
 $map = new Map(5, 5);
-$map->addEntity(new \App\Entity\Coordinate(3, 2), new \App\Entity\Dynamic\Predator\Wolf(100, 3, 5, 4));
-$map->addEntity(new \App\Entity\Coordinate(2, 1), new \App\Entity\Dynamic\Herbivore\Deer(6, 1));
-$map->addEntity(new \App\Entity\Coordinate(2, 0), new \App\Entity\Static\Grass());
-$map->addEntity(new \App\Entity\Coordinate(1, 0), new \App\Entity\Static\Rock());
-$map->addEntity(new \App\Entity\Coordinate(3, 0), new \App\Entity\Static\Tree());
-
-
 $consoleRenderer = new ConsoleRenderer();
 $wayFinder = new BreadthFirstSearch($map);
 
 $spawnRates = json_decode(file_get_contents("../data/spawn_rates.json"), true);
 
 $initActions = [
-//    new TreeSpawnAction($spawnRates['Tree'], $map),
-//    new RockSpawnAction($spawnRates['Rock'], $map),
-//    new DeerSpawnAction($spawnRates['Deer'], $map),
-//    new GrassSpawnAction($spawnRates['Grass'], $map),
-//    new WolfSpawnAction($spawnRates['Wolf'], $map),
+    new TreeSpawnAction($spawnRates['Tree'], $map),
+    new RockSpawnAction($spawnRates['Rock'], $map),
+    new DeerSpawnAction($spawnRates['Deer'], $map),
+    new GrassSpawnAction($spawnRates['Grass'], $map),
+    new WolfSpawnAction($spawnRates['Wolf'], $map),
 ];
 $turnActions = [
     new MoveAction($map, $wayFinder),
